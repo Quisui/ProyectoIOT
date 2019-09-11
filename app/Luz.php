31572d8de6e90964ac85f3cc2fcaac108ee5c75f
Quisui <quiqueal.17@hotmail.com>
2019-09-11 12:32:22 -0600

    Codigo Models
    Se agrego código a modelos para db

--- app/Luz.php
+++ app/Luz.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Luz extends Model
 {
-    //
+    //Uso en la bd
+    protected $table = 'luces';
 }
+
+
+
--- app/Movimiento.php
+++ app/Movimiento.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movimiento extends Model
 {
-    //
+    //Conexion Bd movimiento
+    protected $table = 'movimiento';
 }
