b37b457e6d7208cc1cc28ef3be368402201226c0
Quisui <quiqueal.17@hotmail.com>
2019-09-14 19:04:51 -0600

    Se agregaron las vistas de app
    Se agrego la vista de inicio con la tabla para la bitacora de movimiento y se agregaron los botones para el control de luces de la casa domotica

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>InicioIOT</title>
+
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row justify-content-md-center">
+            <div class="col-auto">
+                <h3 style="font-family: Arial, Helvetica, sans-serif; font: bolder; display: flex; text-align: center ">
+                    Proyecto IOT: Casa Domotica</h3>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid mt-2">
+        <div class="row justify-content-md-center">
+            <div class="col">
+                <h2 style="font-family: 'Times New Roman', Times, serif; text-align: center; font: bolder">Bitacora de
+                    movimiento</h2>
+                <br>
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col">
+                <h2 style="font-family: 'Times New Roman', Times, serif; text-align: center; font: bolder">Control de
+                    Luces</h2>
+                <br>
+                <div class="row">
+                    <div class="col-">
+                        <button type="button" class="btn btn-primary" style="align-self: center">Luces Cocina</button>
+                        <br>
+                        <button type="button" class="btn btn-secondary" style="margin-top: 5px">Luces baño 1</button>
+                    </div>
+                    <div class="col-">
+                        <button type="button" class="btn btn-success" style="margin-left: 5px">Luces Dormitorio
+                            1</button>
+                        <br>
+                        <button type="button" class="btn btn-warning" style="margin-top: 5px; margin-left: 5px">Luces
+                            Dormitorio 2</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
